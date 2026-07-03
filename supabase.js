d7dca97e356fe5be61bc70ca6746d7e07eb19684
Inarius <ninpo.app@gmail.com>
2026-07-03 14:10:11 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://akhezzpdcdlmlbuvpice.supabase.co"
-const supabaseKey = "sb_publishable_uZr6wMVHhSN5otho_n2cUA_-R0aCmTm"
+const supabaseUrl = "https://akhezzpdcdlmlbuvpice.supabase.co";
+const supabaseKey = "sb_publishable_uZr6wMVHhSN5otho_n2cUA_-R0aCmTm";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
