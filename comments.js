5e35aa592ad844e692be1ac543e7f920d81fccb0
Inarius <ninpo.app@gmail.com>
2026-07-03 14:13:13 +0200

    Create comments.js

---
+++ comments.js
@@ -0,0 +1,35 @@
+import { supabase } from "./supabase.js";
+
+// Ajouter un commentaire
+export async function addComment(username, message) {
+  const { error } = await supabase
+    .from("comments")
+    .insert([
+      {
+        username,
+        message
+      }
+    ]);
+
+  if (error) {
+    console.log(error);
+    return "Erreur";
+  }
+
+  return "OK";
+}
+
+
+export async function getComments() {
+  const { data, error } = await supabase
+    .from("comments")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error);
+    return [];
+  }
+
+  return data;
+}
