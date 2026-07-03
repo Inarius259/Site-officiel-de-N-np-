31e69d56850ae7fee688ba567edddaa4182e6c50
Inarius <ninpo.app@gmail.com>
2026-07-03 15:09:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -46,3 +46,84 @@ async function loadComments() {
 }
 
 loadComments();
+
+let rating = 0;
+
+document.querySelectorAll("#stars span").forEach(star => {
+  star.addEventListener("click", () => {
+    rating = Number(star.dataset.value);
+    console.log("rating =", rating);
+  });
+});
+
+
+import { supabase } from "./supabase.js";
+
+const form = document.getElementById("formulaire-avis");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const username = form.querySelector("input").value;
+  const message = form.querySelector("textarea").value;
+
+  const { error } = await supabase
+    .from("commentaires")
+    .insert([
+      {
+        username,
+        message,
+        rating
+      }
+    ]);
+
+  if (!error) {
+    form.reset();
+    loadComments();
+  }
+});
+async function loadComments() {
+  const { data } = await supabase
+    .from("commentaires")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  const top = document.getElementById("topComments");
+  const bottom = document.getElementById("comments");
+
+  top.innerHTML = "";
+  bottom.innerHTML = "";
+
+  data.forEach(c => {
+
+    const box = document.createElement("div");
+
+    box.innerHTML = `
+      <b>${c.username}</b><br>
+      ${c.message}<br>
+      ⭐ ${c.rating ?? 0}/5
+    `;
+
+    box.style.padding = "10px";
+    box.style.margin = "5px";
+    box.style.borderRadius = "10px";
+    box.style.background = "#222";
+    box.style.color = "white";
+
+    // 🔝 petit message → disparaît
+    if (c.message.length < 50) {
+      top.appendChild(box);
+
+      setTimeout(() => {
+        box.remove();
+      }, 5000);
+
+    } 
+    // 📦 gros message → reste
+    else {
+      bottom.appendChild(box);
+    }
+  });
+}
+
+loadComments();
